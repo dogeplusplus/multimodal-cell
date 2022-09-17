31a41aba029dd6256b63f9047261432a396e923e
Albert Chung <albertswchung@gmail.com>
2022-09-17 21:49:37 +0100

    Add function to create submission. Log submissions as mlflow artifacts.

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ import pandas as pd
 
 from pathlib import Path
 from scipy.sparse import csr_matrix
+from tempfile import TemporaryDirectory
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GroupKFold
@@ -162,7 +163,7 @@ def train_full(X_train, Y_train, X_test, lightgbm_params):
 
 
 @timer
-def main():
+def cross_validation():
     DATA_DIR = Path("open_problems_multimodal")
     FP_CELL_METADATA = DATA_DIR / "metadata.csv"
 
@@ -259,5 +260,82 @@ def main():
     mlflow.log_metric("corrscore", np.mean(corrscores))
 
 
+@timer
+def create_submission(lightgbm_params: t.Dict[str, t.Any]):
+    DATA_DIR = Path("open_problems_multimodal")
+    FP_CITE_TRAIN_INPUTS = DATA_DIR / "train_cite_inputs.h5"
+    FP_CITE_TRAIN_TARGETS = DATA_DIR / "train_cite_targets.h5"
+    FP_CITE_TEST_INPUTS = DATA_DIR / "test_cite_inputs.h5"
+    COLUMNS_PATH = Path("multimodal_columns.json")
+
+    with open(COLUMNS_PATH) as f:
+        columns = json.load(f)
+
+    important_cols = columns["important"]
+    constant_cols = columns["constant"]
+
+    X = pd.read_hdf(FP_CITE_TRAIN_INPUTS).drop(columns=constant_cols)
+    X0 = X[important_cols].values
+
+    # SVD performed on the combined train and test set
+    X = csr_matrix(X.values)
+    X_test = pd.read_hdf(FP_CITE_TEST_INPUTS).drop(columns=constant_cols)
+    X0_test = X_test[important_cols].values
+    X_test = csr_matrix(X_test.values)
+
+    combined = scipy.sparse.vstack([X, X_test])
+    assert combined.shape[0] == 119651
+    reduced, _, _ = run_svd(combined)
+
+    X = reduced[:70988]
+    X_test = reduced[70988:]
+    X = np.hstack([X, X0])
+    X_test = np.hstack([X_test, X0_test])
+
+    logger.info(f"Original X shape: {X.shape} {X.size * 4 / (1024 ** 3):2.3f} GByte")
+    logger.info(f"Original X_test shape: {X_test.shape} {X_test.size * (4 / 1024 ** 3):2.3f} GByte")
+    logger.info(f"Reduced X shape: {X.shape} {X.size * 4 / (1024 ** 3):2.3f} GByte")
+    Y = pd.read_hdf(FP_CITE_TRAIN_TARGETS)
+    Y = Y.values
+
+    test_predictions = []
+    y_cols = Y.shape[1]
+
+    for i in range(y_cols):
+        model = lightgbm.LGBMRegressor(**lightgbm_params)
+        model.fit(X, Y[:, i].copy())
+        test_predictions.append(model.predict(X_test))
+
+    test_predictions = np.column_stack(test_predictions)
+    # TODO: Replace with multiome pipeline later.
+    submission = pd.read_csv(
+        Path("open_problems_multimodal", "multiome-sparse-submission.csv"),
+        index_col="row_id",
+        squeeze=True,
+    )
+    submission.iloc[:len(test_predictions.ravel())] = test_predictions.ravel()
+    assert not submission.isna().any()
+
+    mlflow.set_experiment("submissions")
+
+    with TemporaryDirectory() as temp_dir:
+        temp_file = Path(temp_dir) / "submission.csv"
+        submission.to_csv(temp_file)
+        mlflow.log_artifact(temp_file)
+        mlflow.log_params(lightgbm_params)
+
+
 if __name__ == "__main__":
-    main()
+    lightgbm_params = {
+        "learning_rate": 0.1,
+        "max_depth": 10,
+        "num_leaves": 200,
+        "min_child_samples": 250,
+        "colsample_bytree": 0.8,
+        "subsample": 0.6,
+        "seed": 1,
+        "device": "gpu",
+        "verbosity": -1,
+        "n_estimators": 300,
+    }
+    create_submission(lightgbm_params)
