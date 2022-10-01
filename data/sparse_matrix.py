e9dce0893311d62d0fd240961656aaee7cf329c1
Albert Chung <albertswchung@gmail.com>
2022-10-01 12:55:53 +0100

    Save sparse arrays for multiome inputs.

--- data/sparse_matrix.py
+++ data/sparse_matrix.py
@@ -1,20 +1,20 @@
-import logging
 import numpy as np
+import typing as t
 import pandas as pd
 
+from tqdm import tqdm
 from pathlib import Path
+from tempfile import TemporaryDirectory
+from scipy.sparse import hstack, csr_matrix, save_npz
 
-from data.create_indptr import create_indptr
-
-
-logger = logging.getLogger(__name__)
+from create_indptr import create_indptr
 
 
 def check_size(xs: np.ndarray, ys: np.ndarray, datas: np.ndarray) -> float:
     return (xs.nbytes + ys.nbytes + datas.nbytes) * 1e-9
 
 
-def create_csr_arrays(h5_file_path: Path):
+def create_csr_arrays(h5_file_path: Path, destination: Path) -> t.Tuple[t.List[Path], t.List[Path], t.List[Path]]:
     # Initialize Variables
     chunksize = 1000
     loaded_rows = chunksize
@@ -27,15 +27,26 @@ def create_csr_arrays(h5_file_path: Path):
     indices = np.array([], dtype=np.int32)
     data_s = np.array([], dtype=np.float32)
 
-    prefix_filename = h5_file_path.name
+    indptr_paths = []
+    indice_paths = []
+    data_paths = []
 
+    pbar = tqdm()
     while chunksize == loaded_rows:
         size_gb = check_size(indptr, indices, data_s)
         if size_gb > 7.0:
-            logger.info(f"Total size is {size_gb}. Saving...")
-            np.save(f"{prefix_filename}_indptr_{file_pointer}.npy", indptr)
-            np.save(f"{prefix_filename}_indices_{file_pointer}.npy", indices)
-            np.save(f"{prefix_filename}_data_{file_pointer}.npy", data_s)
+            pbar.set_postfix_str(f"Total size is {size_gb}. Saving...")
+            indptr_path = destination / f"indptr_{file_pointer}.npy"
+            indice_path = destination / f"indice_{file_pointer}.npy"
+            data_path = destination / f"data_{file_pointer}.npy"
+
+            np.save(indptr_path, indptr)
+            np.save(indice_path, indices)
+            np.save(data_path, data_s)
+
+            indptr_paths.append(indptr_path)
+            indice_paths.append(indice_path)
+            data_paths.append(data_path)
 
             # Re-initialize
             indptr = np.array([], dtype=np.int64)
@@ -43,9 +54,9 @@ def create_csr_arrays(h5_file_path: Path):
             data_s = np.array([], dtype=np.float32)
             file_pointer += 1
 
-        logger.info("Reading .h5 chunk")
+        pbar.set_postfix_str("Reading .h5 chunk")
         df = pd.read_hdf(h5_file_path, start=start, stop=start+chunksize)
-        logger.info("Extracting non-zero values")
+        pbar.set_postfix_str("Extracting non-zero values")
         x_coords, y_coords = df.values.nonzero()
         tmp_data = df.values[df.values != 0.0]
 
@@ -55,18 +66,77 @@ def create_csr_arrays(h5_file_path: Path):
         y_coords = y_coords.astype(np.int32, copy=False)
         tmp_data = tmp_data.astype(np.float32, copy=False)
 
-        logger.info("Compressing row values")
+        pbar.set_postfix_str("Compressing row values")
         x_coords = create_indptr(x_coords, start_pos=start_pos, nrows=loaded_rows)
 
-        logger.info("Update variables")
+        pbar.set_postfix_str("Update variables")
         start_pos += y_coords.shape[0]
         start += chunksize
+
         # Append data at the end of each array
         indptr = np.hstack((indptr, x_coords))
         indices = np.hstack((indices, y_coords))
         data_s = np.hstack((data_s, tmp_data))
 
-    logger.info("Done. Save last files")
-    np.save(f"{prefix_filename}_indptr_{file_pointer}.npy", indptr)
-    np.save(f"{prefix_filename}_indices_{file_pointer}.npy", indices)
-    np.save(f"{prefix_filename}_data_{file_pointer}.npy", data_s)
+        pbar.update(loaded_rows)
+
+    pbar.set_postfix_str("Done. Save last files")
+    indptr_path = destination / f"indptr_{file_pointer}.npy"
+    indice_path = destination / f"indice_{file_pointer}.npy"
+    data_path = destination / f"data_{file_pointer}.npy"
+
+    np.save(indptr_path, indptr)
+    np.save(indice_path, indices)
+    np.save(data_path, data_s)
+
+    indptr_paths.append(indptr_path)
+    indice_paths.append(indice_path)
+    data_paths.append(data_path)
+
+    return indptr_paths, indice_paths, data_paths
+
+
+def create_sparse_matrix(
+    indptr_paths: t.List[Path],
+    indice_paths: t.List[Path],
+    data_paths: t.List[Path],
+) -> csr_matrix:
+    # Known shapes of the multiome inputs apriori
+    N_ROWS = 105942
+    N_COLS = 228942
+
+    csr_arrays = []
+    for ptr, ind, dat in zip(indptr_paths, indice_paths, data_paths):
+        indptr = np.load(ptr)
+        indices = np.load(ind)
+        data = np.load(dat)
+
+        # Indptr has shape nrows instead of nrows + 1, can add last elemenent
+        # corresponding to the length of indices or data arrays
+        indptr = np.append(indptr, indptr[-1] + indices[indptr[-1]:].shape)
+        csr = csr_matrix((data, indices, indptr), shape=(N_ROWS, N_COLS))
+        csr_arrays.append(csr)
+
+    return hstack(csr_arrays)
+
+
+def main():
+    DATA_DIR = Path("multimodal")
+    TRAIN_MULTI_INPUTS = DATA_DIR / "train_multi_inputs.h5"
+    TEST_MULTI_INPUTS = DATA_DIR / "test_multi_inputs.h5"
+
+    TRAIN_OUTPUT_PATH = DATA_DIR / "train_multiome_input_sparse.npz"
+    TEST_OUTPUT_PATH = DATA_DIR / "test_multiome_input_sparse.npz"
+
+    for source, destination in [
+        (TRAIN_MULTI_INPUTS, TRAIN_OUTPUT_PATH),
+        (TEST_MULTI_INPUTS, TEST_OUTPUT_PATH),
+    ]:
+        with TemporaryDirectory() as temp_dir:
+            indptr_paths, indice_paths, data_paths = create_csr_arrays(source, Path(temp_dir))
+            stacked_csr = create_sparse_matrix(indptr_paths, indice_paths, data_paths)
+        save_npz(destination, stacked_csr)
+
+
+if __name__ == "__main__":
+    main()
